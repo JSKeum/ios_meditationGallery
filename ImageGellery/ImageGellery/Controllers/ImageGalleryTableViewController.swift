9c1c0a20abb9605bdcb10bb6175a069608a535b4
JSKeum <JSKeum2512@gmail.com>
2020-03-09 19:40:52 +0900

    Table View Cell add & delete with animation - key: using BatchUpdate instead of reloadData

--- ImageGellery/ImageGellery/Controllers/ImageGalleryTableViewController.swift
+++ ImageGellery/ImageGellery/Controllers/ImageGalleryTableViewController.swift
@@ -20,16 +20,18 @@ class ImageGalleryTableViewController: UITableViewController {
     private var gallerySection: Int? {
         return sections.firstIndex(of: "Gallery List")
     }
+    
     private var deletedSection: Int? {
         return sections.firstIndex(of: "Unused List")
     }
     
-    
     @IBAction func addGallery(_ sender: UIBarButtonItem) {
         let newTitle = "newGallery"
-        galleryList += [newTitle]
         
-        tableView.reloadData()
+        tableView.performBatchUpdates({
+            galleryList += [newTitle]
+            tableView.insertRows(at: [IndexPath(row: galleryList.count-1, section: gallerySection!)], with: .top)
+        })
     }
     
     override func viewDidLoad() {
@@ -62,13 +64,11 @@ class ImageGalleryTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-
+        
         // custom cell
         if let titleCell = cell as? ImageGalleryTableViewCell {
             titleCell.title?.text = (indexPath.section == gallerySection) ? galleryList[indexPath.row] : unusedGalleryList[indexPath.row]
-            
         }
-
         
         return cell
     }
@@ -77,24 +77,28 @@ class ImageGalleryTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
-        if (indexPath.section == gallerySection) {
-            
+            if (indexPath.section == gallerySection) {
                 let deletedItem = galleryList[indexPath.row] ?? nil
-                galleryList.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
                 
-                // model 변경시에 항상 reloaddata 해줘야 한다. 아니면 오류남
-                unusedGalleryList += [deletedItem]
-                tableView.reloadData()
+                // WWDC 2018 Collection View 예제에서는 먼저 reloadItems를 해줬지만, 왠지 안하고 performBatch 하나만 해도 된다. Collection View 에서도 그럴까?
+                
+                tableView.performBatchUpdates({
+                    
+                    galleryList.remove(at: indexPath.row)
+                    unusedGalleryList.insert(deletedItem, at: 0)
+                           
+                     tableView.moveRow(at: indexPath, to: IndexPath(row: 0, section: deletedSection!))
+                })
             }
+                
             else if (indexPath.section == deletedSection) {
                 
                 unusedGalleryList.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .bottom)
             }
-            
         }
     }
+    
     // *** stored properties used in extensions
     private var beforeIndexPath: IndexPath? = nil
 }
@@ -111,22 +115,22 @@ extension ImageGalleryTableViewController {
 
 // segue
 extension ImageGalleryTableViewController {
- 
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "galleryAction" {
-//            if let titleName = (sender as? ImageGalleryTableViewCell)?.title {
-                if let cvc = segue.destination as? ImageGalleryViewController {
-//                    
-                }
-//            }
+            //            if let titleName = (sender as? ImageGalleryTableViewCell)?.title {
+            if let cvc = segue.destination as? ImageGalleryViewController {
+                //
+            }
+            //            }
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let beforeIdxPath = beforeIndexPath {
-        tableView.deselectRow(at: beforeIdxPath, animated: true)
+            tableView.deselectRow(at: beforeIdxPath, animated: true)
         }
-//        let cell = tableView.cellForRow(at: indexPath)
+        //        let cell = tableView.cellForRow(at: indexPath)
         performSegue(withIdentifier: "galleryAction", sender: self)
         
         beforeIndexPath = indexPath
